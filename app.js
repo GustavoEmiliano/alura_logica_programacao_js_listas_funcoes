c5276bea2ddebf4c2248a21dbaf8f10649e9c420
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-09-16 21:20:24 -0300

    add: função em JavaScript

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ titulo.innerHTML = "Jogo do número secreto";
 
 let paragrafo = document.querySelector("p");
 paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+/ * Uma função é responsável por determinar alguma ação dentro do nosso código */
+function verificarChute(){
+  console.log("O botão foi clicado!"); 
+}
