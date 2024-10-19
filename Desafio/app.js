933061e3aa78a61f56b54d148239445d27ce172e
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-10-19 19:13:27 -0300

    add: desafio sobre listas

---
+++ Desafio/app.js
@@ -0,0 +1,20 @@
+// Exercício 1: Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// Exercício 2: Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript', 'C', 'C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// Exercício 3: Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+// Exercício 4: Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaNomes = ['Janiely', 'Gustavo', 'Emiliano'];
+console.log(listaNomes[0]);
+
+// Exercício 5: Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let listaNomes2 = ['Gabriel', 'Oliver', 'Dante'];
+console.log(listaNomes2[1]);
+
+// Exercício 6: Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let listaNomes3 = ['Sebastian', 'Rodriguez', 'Valentino'];
+console.log(listaNomes3[listaNomes3.length - 1]);
