42ca974274023e30da4c4ee9a495cd48d29f131f
Gustavo Emiliano <gustavoemiliano2014@gmail.com>
2024-10-19 11:58:04 -0300

    add: clearing the text field after each guess and enabling the (New Game) button when the player guesses the secret number

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela("h1", "Jogo do número secreto");
 exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 
-
 function verificarChute() {
   let chute = parseInt(document.querySelector("input").value);
 
   if (chute == numeroSecreto) {
     exibirTextoNaTela("h1", "Acertou");
-    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
     let mensagemTentativas = `Você descobriu o número Secreto com ${tentativas} ${palavraTentativa}!`;
     exibirTextoNaTela("p", mensagemTentativas);
+    document.getElementById("reiniciar").removeAttribute("disabled");
   } else {
     if (chute > numeroSecreto) {
       exibirTextoNaTela("p", "O número secreto é menor");
@@ -25,9 +25,15 @@ function verificarChute() {
       exibirTextoNaTela("p", "O número secreto é maior");
     }
     tentativas++;
+    limparCampo();
   }
 }
 
 function gerarNumeroAleatorio() {
   return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+  chute = document.querySelector("input");
+  chute.value = "";
 }
